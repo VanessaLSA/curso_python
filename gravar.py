ac3715a9794d990b90558dda2b4e387f9f645979
VanessaLSA <159334684+VanessaLSA@users.noreply.github.com>
2024-04-08 17:01:44 -0300

    Add files via upload

--- gravar.py
+++ gravar.py
@@ -1,12 +1,13 @@
 import csv
 import tkinter as tk
 from tkinter import messagebox
-
+import os
 
 
 def gravar_contato():
+
     if entry_nome.get() == "" or entry_fone.get() == "" or entry_email.get() == "":
-        messagebox.showerror("Erro ao gravar! ", "Favor preencher todos os dados! ")
+        messagebox.showerror("Erro ao gravar", "Todos os campos devem ser preenchidos!")
     else:
         with open("dados.csv", "a", newline="") as arquivo_dados:
             escritor = csv.writer(arquivo_dados)
@@ -14,6 +15,7 @@ def gravar_contato():
             messagebox.showinfo("Sistema contatos", "Contato cadastrado com sucesso!")
             limpar_campos()
 
+        ler_contatos()
 
 
 def limpar_campos():
@@ -23,21 +25,17 @@ def limpar_campos():
     entry_nome.focus_set()
 
 
-
-    ler_contatos()
-
-
 def ler_contatos():
     with open("dados.csv", "r") as arquivo_dados:
         leitor = csv.reader(arquivo_dados)
-        lista_contatos.delete(0, tk.END) #limpar a lista
+        lista_contatos.delete(0, tk.END)# limpar a lista
         for linha in leitor:
             lista_contatos.insert("end", linha[0])
 
 
-def buscar_contatos_pelo_indice(indice_procurado):
-    with (open("dados.csv", "r") as arquivos_dados):
-        leitor = csv.reader(arquivos_dados)
+def buscar_contato_pelo_indice(indice_procurado):
+    with open("dados.csv", "r") as arquivo_dados:
+        leitor = csv.reader(arquivo_dados)
         volta = 0
         for linha in leitor:
             if volta == indice_procurado:
@@ -51,15 +49,38 @@ def buscar_contatos_pelo_indice(indice_procurado):
 def obter_indice(event):
     indice = lista_contatos.curselection()[0]
     limpar_campos()
-    buscar_contatos_pelo_indice(indice)
+    buscar_contato_pelo_indice(indice)
 
 
+def excluir_contato():
+
+    resposta = messagebox.askokcancel("Excluir Contato", "Tem certeza de que deseja excluir o contato selecionado!")
+
+    if resposta:
+        with open("dados.csv", "r") as arquivo_dados, open("temp.csv", "a", newline="") as arquivo_temp:
+            leitor = csv.reader(arquivo_dados)
+            escritor = csv.writer(arquivo_temp)
+
+            for contato in leitor:
+                if entry_nome.get() != contato[0] and entry_fone.get() != contato[1] and entry_email.get() != contato[2]:
+                    escritor.writerow([contato[0], contato[1], contato[2]])
+
+        # apagar o dados.csv
+        os.remove("dados.csv")
+
+        # renomear o temp.csv para dados.csv
+        os.rename("temp.csv", "dados.csv")
+
+        limpar_campos()
+        ler_contatos()
+
+    else:
+        messagebox.showinfo("Excluir contato", "Operação cancelada pelo usuário!")
 
 
 janela = tk.Tk()
 
 janela.geometry("580x300")
-
 
 label_nome = tk.Label(janela, text="Nome:")
 label_fone = tk.Label(janela, text="Telefone:")
@@ -70,7 +91,8 @@ entry_nome = tk.Entry(janela)
 entry_fone = tk.Entry(janela)
 entry_email = tk.Entry(janela)
 
-button_gravar = tk.Button(text="Gravar contato", command=gravar_contato)
+button_gravar = tk.Button(text="Salvar", command=gravar_contato)
+button_excluir = tk.Button(text="Excluir", command=excluir_contato)
 
 lista_contatos = tk.Listbox(janela, selectmode="single")
 lista_contatos.bind("<<ListboxSelect>>", obter_indice)
@@ -88,31 +110,20 @@ label_fone.place(x=10, y=80)
 entry_fone.config(font=("Arial", 16))
 entry_fone.place(x=10, y=110, width=300, height=30)
 
-
 label_email.config(font=("Arial", 16))
 label_email.place(x=10, y=150)
 entry_email.config(font=("Arial", 16))
 entry_email.place(x=10, y=180, width=300, height=30)
 
+button_gravar.config(font=("Arial", 16))
+button_gravar.place(x=10, y=230, width=145, height=60)
 
+button_excluir.config(font=("Arial", 16))
+button_excluir.place(x=165, y=230, width=145, height=60)
 
-
-button_gravar.config(font=("Arial",16))
-button_gravar.place(x=10, y=230, width=300, height=60)
-
-
-lista_contatos.config(font=("Arial",16))
+lista_contatos.config(font=("Arial", 16))
 lista_contatos.place(x=320, y=40, width=250)
 
 ler_contatos()
 
-
-
-
-
-
-
-
 janela.mainloop()
-
-
--- leitor.py
+++ leitor.py
@@ -4,3 +4,5 @@ with open("dados.csv", "r") as arquivo_dados:
     leitor = csv.reader(arquivo_dados)
     for linha in leitor:
         print(f"{linha[0]:<20} {linha[2]:<20} {linha[1]:>20}")
+
+
